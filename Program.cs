814ce1f1ce72abe004288bfb5c681b2c34112611
Leomar Alves Marcelino <leo.dev.jobs@gmail.com>
2026-09-07 02:57:01 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿public class Pedido
+{
+    public string Cliente {get; set; }
+    public decimal Valor { get; set; }
+
+    public bool EhValido()
+    {
+        return Valor > 0 && !string.IsNullOrWhiteSpace(Cliente);
+    }
+}
+
+public interface INotificador
+{
+    void EnviarMensagem(string destinatario, string mensagem);
+}
+
+public class NotificadorConsole : INotificador
+{
+    public void EnviarMensagem(string destinatario, string mensagem)
+    {
+        Console.WriteLine($"[Notificação] Para {destinatario}: {mensagem}")
+    }
+}
+
+public class ProcessadorDePedido
+{
+    private readonly INotificador _notificador;
+
+    public ProcessadorDePedido(INotificador notificador)
+    {
+        -notificador = notificador;
+    }
+}
+
